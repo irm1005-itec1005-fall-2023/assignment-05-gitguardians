7eeeb285c48e0f60874932ad18cf72a7e2c84dd4
Henri Linkletter <henrilinkletter@gmail.com>
2023-11-25 20:09:20 -0500

    bomb function and temp files

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-
 let Xarray = [];
 
-
-//minesweeper entities
 let tile = {
- isbomb: false,
- isnearby: 0,
-isrevealed: false,
- isflagged: false,
-xcord: 0,
-ycord: 0,
-
+    isbomb: false,
+    isnearby: 0,
+    isrevealed: false,
+    isflagged: false,
+    xcord: 0,
+    ycord: 0,
 }
 
-for (let x = 0; x <= 14; x++) {
+for (let x = 0; x < 14; x++) {
 
- Xarray[x] = [];
+    Xarray[x] = [];
 
-    for (let y = 0; y <= 14; y++) {
-
-        let temptile = tile;
+    for (let y = 0; y < 14; y++) {
+        let temptile = { ...tile };
         temptile.xcord = x;
         temptile.ycord = y;
+        console.log(temptile);
         Xarray[x].push(temptile);
     }
 }
 
+console.log(Xarray);
 
+console.log(Math.random());
+
+for (x = 0; x < 14; x++) {
+    for (y = 0; y < 14; y++) {
+        if (Math.random() < 0.2) {
+            Xarray[x][y].isbomb = true;
+        }
+    }
+}
