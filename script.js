cdf7b55f2064d1e382f8f3fa684aec4580e1c3bb
Henri Linkletter <henrilinkletter@gmail.com>
2023-11-26 15:25:50 -0500

    temp css grid update

--- script.js
+++ script.js
@@ -10,39 +10,36 @@ let tile = {
     ycord: 0,
 }
 
-for (let x = 0; x < 14; x++) {
+//for (let x = 0; x < 14; x++) {
+//
+//    Xarray[x] = [];
+//
+//    for (let y = 0; y < 14; y++) {
+//        let temptile = { ...tile };
+//        let tempVisualTile = document.createElement("li");
+//        temptile.xcord = x;
+//        temptile.ycord = y;
+//        console.log(temptile);
+//        Xarray[x].push(temptile);
+//        tempVisualTile = temptile;
+//        minesweepergrid.appendChild(tempVisualTile);
+//    }
+//}
 
-    Xarray[x] = [];
-
-    for (let y = 0; y < 14; y++) {
-        let temptile = { ...tile };
-        let tempVisualTile = document.createElement("li");
-        temptile.xcord = x;
-        temptile.ycord = y;
-        console.log(temptile);
-        Xarray[x].push(temptile);
-        tempVisualTile = temptile;
-        minesweepergrid.appendChild(tempVisualTile);
-    }
-}
-
-console.log(Xarray);
+// console.log(Xarray);
 
 console.log(Math.random());
 
-for (x = 0; x < 14; x++) {
-    for (y = 0; y < 14; y++) {
-        if (Math.random() < 0.2) {
-            Xarray[x][y].isbomb = true;
-        }
-    }
-}
+// for (x = 0; x < 14; x++) {
+//     for (y = 0; y < 14; y++) {
+//         if (Math.random() < 0.2) {
+//             Xarray[x][y].isbomb = true;
+//         }
+//     }
+// }
 
-let tempPokemon =
-  `
-  <li class="pokemon-list-item">
-    <h3 class="pokemon-list-item-name">${pokemons[i].name}</h3>
-    <p class="pokemon-list-item-level">Level: ${pokemons[i].level} </p>
-    <p class="pokemon-list-item-type">Type: ${pokemons[i].type}</p>
-  </li>
-  `
+for (x = 0; x < 196; x++) {
+    let tempVisualTile = document.createElement("li");
+    tempVisualTile.textContent = x;
+    minesweepergrid.appendChild(tempVisualTile);
+}
