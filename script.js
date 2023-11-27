712a6d780e62e9c6e8be0a4c1acb19551477d531
Henri Linkletter <henrilinkletter@gmail.com>
2023-11-26 19:06:32 -0500

    added some shit to the script, also added to temp

--- script.js
+++ script.js
@@ -10,36 +10,41 @@ let tile = {
     ycord: 0,
 }
 
-//for (let x = 0; x < 14; x++) {
-//
-//    Xarray[x] = [];
-//
-//    for (let y = 0; y < 14; y++) {
-//        let temptile = { ...tile };
-//        let tempVisualTile = document.createElement("li");
-//        temptile.xcord = x;
-//        temptile.ycord = y;
-//        console.log(temptile);
-//        Xarray[x].push(temptile);
-//        tempVisualTile = temptile;
-//        minesweepergrid.appendChild(tempVisualTile);
-//    }
-//}
+for (let x = 0; x < 14; x++) {
 
-// console.log(Xarray);
+    Xarray[x] = [];
 
-console.log(Math.random());
-
-// for (x = 0; x < 14; x++) {
-//     for (y = 0; y < 14; y++) {
-//         if (Math.random() < 0.2) {
-//             Xarray[x][y].isbomb = true;
-//         }
-//     }
-// }
-
-for (x = 0; x < 196; x++) {
-    let tempVisualTile = document.createElement("li");
-    tempVisualTile.textContent = x;
-    minesweepergrid.appendChild(tempVisualTile);
+    for (let y = 0; y < 14; y++) {
+        let temptile = { ...tile };
+        temptile.xcord = x;
+        temptile.ycord = y;
+        console.log(temptile);
+        Xarray[x].push(temptile);
+        tempVisualTile = temptile;
+    }
 }
+
+// Adds bombs to each array element with a 20% chance of being a bomb 
+for (x = 0; x < 14; x++) {
+    for (y = 0; y < 14; y++) {
+        if (Math.random() < 0.2) {
+            Xarray[x][y].isbomb = true;
+        }
+    }
+}
+
+console.log(Xarray[0][2]);
+
+// This function creates an li and a button for each tile, also giving them the id of "tile". It then adds them to the list in the html file. The CSS displays them in a grid. 
+for (x = 0; x < 14; x++) {
+    for (y = 0; y < 14; y++) {
+        let tempVisualTile = document.createElement("li");
+        let tempButton = document.createElement("button");
+        tempButton.id = "tile";
+        tempButton.textContent = " ";
+        tempVisualTile.appendChild(tempButton);
+        minesweepergrid.appendChild(tempVisualTile);
+    }
+}
+
+
